6c07b058d8a53ab37996658ccb27beafa106cec2
YHWH-333 <47367157+YHWH-333@users.noreply.github.com>
2019-02-14 15:20:31 -0500

    Create Animal.scala

---
+++ animals/Animal.scala
@@ -0,0 +1,11 @@
+// Animals: Mobile living beings with souls.
+
+import com.god.Soul;
+
+trait Animal {
+    var soul: Soul;
+    var age: Double;
+}
+
+// TODO: Add more necessary traits of an "Animal"
+// Turns out they vary LARGELY.
